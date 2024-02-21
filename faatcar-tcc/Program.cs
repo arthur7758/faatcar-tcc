9d8e8de8c83e4ac4610d009ad9dc9c07b61a3a10
Arthur <arthurgiacomini4321@outlook.com>
2024-02-21 13:31:14 -0300

    Sou arthur G. Foi alterado a conexão com banco, criado uma extensão para o banco, instalado pacotes. ainda tentando fazer a conexão.

--- faatcar-tcc/Program.cs
+++ faatcar-tcc/Program.cs
@@ -1,7 +1,15 @@
+using faatcar_tcc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BancoContext>(options=>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
