e4c7859e155c3d9aeabe750b044677749a7afdf7
user <user@DESKTOP-6MGCR2D>
2024-02-21 11:35:17 -0300

    Adicionar arquivos de projeto.

---
+++ faatcar-tcc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace faatcar_tcc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
